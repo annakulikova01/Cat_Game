dd032c4f25f137476887d3a063bbd663cd5a96d6
Anna Kulikova <akulikov@phoenix.goucher.edu>
2022-12-14 01:57:48 -0500

    fixed random coords and added dog_gui class

--- src/GUI.java
+++ src/GUI.java
@@ -470,7 +470,7 @@ public class GUI extends Observable implements Observer, LineListener {
 							int ymin = 100;
 							int ymax = 500;
 							int y = ymin + (int) (Math.random() * (ymax - ymin));	
-							
+							i.setBounds(x, y, i.getIconWidth(), i.getIconHeight());
 							i.setVisible(true);
 							i.setVisibility(true);
 							
